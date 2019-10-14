f0e4931fd46123081f5b9041e0912de16258e2bb
Kthom1 <keenan.thompson67@gmail.com>
2019-10-14 18:23:08 +0800

    Implemented Show component

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Show from "./components/Show";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const client = new ApolloClient();
-
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/edit/:id" component={Edit} />
+        <Route path="/create" component={Create} />
+        <Route path="/show/:id" component={Show} />
+      </div>
+    </Router>
   </ApolloProvider>,
   document.getElementById("root")
 );
